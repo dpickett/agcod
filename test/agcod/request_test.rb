59581103ba42cc6d601e10e67fb5692e75c67747
Bill Rowell <bill.rowell@launchware.com>
2012-12-19 14:48:53 -0500

    test a non error failure

--- test/agcod/request_test.rb
+++ test/agcod/request_test.rb
@@ -27,4 +27,19 @@ class Agcod::RequestTest < Test::Unit::TestCase
       assert_equal "SUCCESS", @request.status
     end
   end
+
+  context 'handle for a non-error failure' do
+    setup do
+      Agcod::Configuration.load(File.join(File.dirname(__FILE__), "..", "app_root"), "test")
+      @request = Agcod::CreateGiftCard.new('value' => 13.57, 'request_id' => 12345)
+    end
+
+    should 'store failure from statusMessage' do
+      uri = URI.parse(@request.request_url)
+      register_response %r{^#{ uri.scheme }://#{ uri.host }}, %w( create_gift_card/non-error-failure create_gift_card/success )
+      @request.submit
+      assert_equal "FAILURE", @request.status
+      assert(@request.errors.count > 0)
+    end
+  end
 end
